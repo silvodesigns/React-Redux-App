0df35275727734271d002d02abe2f9defbe886cb
silvodesigns <kevsilvo@gmail.com>
2019-10-27 12:47:49 -0400

    added redux

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
 import App from './App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function reducer(){
+    return{
+        state: "this is my state"
+    }
+}
+
+const store = createStore(reducer);
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
